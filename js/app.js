8bb5ccd9383bac9b97222cf1fb72385f19cec83d
Moksleivis <domas.pocius@gmail.com>
2020-10-23 14:55:56 +0300

    pirmas

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/*
+ Komentaras.
+ */
+/*let name = prompt( 'iveskite savo varda');
+console.log("Ivestas vardas:"+name);
+let items = ['item1' , 10, true, {name:"domas"}, 'penktadienis']; // array
+let addItem = items.push('Super item');
+items.pop();
+
+for (let i=0;i<items.length;i++){
+    console.log(i+"-"+items[i])
+}*/
+let kiekis = prompt('Kiek bus sveciu?');
+let svecias;
+for (let i=0; i<kiekis;i++){
+    svecias =prompt('svecio vardas');
+    console.log(svecias);
+}
+
+
+
